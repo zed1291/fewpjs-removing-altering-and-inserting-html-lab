fe8d55c6fad3cc6415a02799e32f1fa724c30147
“zed” <zed@kolk.me>
2021-02-24 09:57:43 -0600

    commit tree ca72bf44088905958f58f31b55b10b4000c3d1a5

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here!
+let element = document.createElement('div')
+document.body.appendChild(element)
+
+let ul = document.createElement('ul')
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+
+element.appendChild(ul)
+
+element.style.backgroundColor = '#27647B';
+element.style.textAlign = 'center';
+ul.style.textAlign = 'left'
+
+//my code
+document.querySelector('main#main').remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Zed is the champion'
